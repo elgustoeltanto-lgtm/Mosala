5d88aa724de2ad04b9e8770545c9cebd7830cb69
elgustoeltanto-lgtm <elgustoeltanto@gmail.com>
2026-09-13 22:25:31 +0200

    Ajout du dossier backend pour les API Mobile Money

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mosala.app',
+  appName: 'Mosala',
+  webDir: 'dist'
+};
+
+export default config;
